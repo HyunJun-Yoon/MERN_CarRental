bbd7a7d9583d4bdee484541222edfc7c79119812
Hyunjun Yoon <yhj940719@gmail.com>
2021-06-21 17:07:09 -0400

    nav bar & calendar added

---
+++ src/app/containers/HomePage/bookingSteps.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const Container = styled.div`
+  ${tw`
+        w-full
+        flex
+        flex-col
+        items-center
+    `}
+`;
+
+export function BookingSteps() {}
---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import tw from 'twin.macro';
+import styled from 'styled-components';
+import { Navbar } from '../../components/navbar';
+import { TopSection } from './topSection';
+import { BookCard } from '../../components/bookCard';
+import { Marginer } from '../../components/marginer';
+
+const PageContainer = styled.div`
+  ${tw`
+        flex
+        flex-col
+        w-full
+        h-full
+        items-center
+        overflow-x-hidden      
+    `}
+`;
+
+export function HomePage() {
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+      <Marginer direction='vertical' margin='4em' />
+      <BookCard />
+    </PageContainer>
+  );
+}
